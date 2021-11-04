f61509454d4c115a9487e3dde87e73a273968619
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-11-04 01:24:22 -0300

    Adicionando script de CRUD e recuperando dados apenas pendentes

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+	$acao = 'recuperarTarefasPendentes';
+
+	require_once 'tarefa_controller.php';
+?>
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -7,6 +12,49 @@
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+			function editar(id, txt_tarefa){
+				let form =  document.createElement('form')
+					form.action = 'index.php?pag=index&acao=atualizar'
+					form.method = 'POST'
+					form.className = 'row'
+
+				let inputTarefa = document.createElement('input')
+					inputTarefa.type = 'text'
+					inputTarefa.name = 'tarefa'
+					inputTarefa.className = 'col-9 form-control'
+					inputTarefa.value = txt_tarefa
+				
+				let inputId = document.createElement('input')
+					inputId.type = 'hidden'
+					inputId.name = 'id'
+					inputId.value = id
+
+				let button = document.createElement('button')
+					button.type = 'submit'
+					button.className = ' col-3 btn btn-info'
+					button.innerHTML = 'Atualizar'
+
+				form.appendChild(inputTarefa)
+				form.appendChild(inputId)
+				form.appendChild(button)
+
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				tarefa.innerHTML = ''
+
+				tarefa.insertBefore(form, tarefa[0])
+
+			}
+
+			function remover(id){
+				location.href = 'index.php?pag=index&acao=remover&id='+id
+			}
+			function marcarRealizada(id){
+				location.href = 'index.php?pag=index&acao=marcarRealizada&id='+id
+			}
+		</script>
 	</head>
 
 	<body>
@@ -36,23 +84,17 @@
 								<h4>Tarefas pendentes</h4>
 								<hr />
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+						<?php foreach($tarefas as $indice => $tarefa){ ?>
+									<div class="row mb-3 d-flex align-items-center tarefa">
+										<div class="col-sm-9" id="tarefa_<?php echo $tarefa->id; ?>"> <?php echo $tarefa->tarefa ;?></div>
+										<div class="col-sm-3 mt-2 d-flex justify-content-between">
+											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?php echo $tarefa->id ?>)"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->tarefa ?>') "></i>
+											<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?php echo $tarefa->id ?>)"></i>
+										</div>
 									</div>
-								</div>
+						<?php } ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
